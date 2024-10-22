88040ecbf9031b2eed3d540e483eb65a3b8c5f2a
Giahuy2502 <huy1kicm@gmail.com>
2024-10-22 16:15:38 +0700

    Add Tutorial Panel

---
+++ Assets/Scripts/Panel/TutorialPanelManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPanelManager : MonoBehaviour
+{
+    public void OnOkButton()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
