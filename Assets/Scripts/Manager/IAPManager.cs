08eff2e7241aa1dd0cdd4e17e45252914e21d3a9
Giahuy2502 <huy1kicm@gmail.com>
2024-11-06 17:26:07 +0700

    fix bug IAP Intiinitialized

---
+++ Assets/Scripts/Manager/IAPManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Unity.Services.Core;
+using Unity.Services.Core.Environments;
+using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Extension;
+
+public class IAPManager : MonoBehaviour, IDetailedStoreListener
+{
+    public string environment = "production";
+    private IStoreController controller;
+    private IExtensionProvider extensions;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        InitGamingServices();
+        InitIAPSystem();
+    }
+
+    private async void InitGamingServices()
+    {
+        try
+        {
+            Debug.Log($"--- (IAP) Initializing Gaming Services...");
+
+            var options = new InitializationOptions()
+                .SetEnvironmentName(environment);
+
+            await UnityServices.InitializeAsync(options);
+        }
+        catch (Exception exception)
+        {
+            Debug.Log($"--- (IAP) Initializing Gaming Services failed --- {exception.Message}");
+            // An error occurred during services initialization.
+        }
+    }
+
+    public void InitIAPSystem()
+    {
+        Debug.Log($"--- (IAP) Initializing IAP...");
+
+        var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
+        builder.AddProduct("option_1", ProductType.Consumable);
+
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+    /// <summary>
+    /// Called when Unity IAP is ready to make purchases.
+    /// </summary>
+    public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        Debug.Log($"--- (IAP) Initializing IAP success");
+
+        this.controller = controller;
+        this.extensions = extensions;
+    }
+
+    public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureDescription failureDescription)
+    {
+     
+    }
+    
+    public void OnInitializeFailed(InitializationFailureReason error)
+    {
+        Debug.Log($"--- (IAP) Initializing IAP failed --- {error}");
+    }
+
+    public void OnInitializeFailed(InitializationFailureReason error, string message)
+    {
+        Debug.Log($"--- (IAP) Initializing IAP failed --- {error} --- {message}");
+    }
+
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+    {
+       
+    }
+}
